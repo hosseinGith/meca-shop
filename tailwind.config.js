5b3d742d22b748ac1c8901897e621462715f325e
hosseinGith <hosseinderee7@gmail.com>
2024-12-28 16:45:42 +0330

    set checkout products to swiper;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html", "./dist/*.js"],
+  content: ["./dist/*.html"],
   safelist: [
     "storySlider",
     "mx-auto",
